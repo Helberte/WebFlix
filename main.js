7c3eade0502db10628d2f6a830939519ef7cab59
helberte costa arruda <helberteads@gmail.com>
2022-09-09 15:56:19 -0400

    Versão 1.0.0 - estrutura inicial

---
+++ main.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const nunjucks = require('nunjucks');
+
+
+nunjucks.configure('views', {
+  autoescape: true,
+  noCache: true,
+  express: app
+})
+
+app.set('view engine', 'html');
+app.use(express.static('public'));
+
+
+app.get('/',  (req, res) => {
+  res.render('index',{
+    nome: 'Webflix'
+  })
+})
+
+
+app.listen(8000, erro => {
+  if(erro){
+    console.log('Deu erro');
+  }else{
+    console.log('Servidor levantado');
+  }
+})
