91c7546d8b85524dbabae9e1f0f77d9f7686ba93
helberte <helberteads@gmail.com>
2022-11-09 21:32:54 -0400

    Criando migrações e o banco de dados

--- main.js
+++ main.js
@@ -3,19 +3,6 @@ const app = express();
 const nunjucks = require('nunjucks');
 
 // rotas
-// const base = require('./routes/base');
-// const configurar = require('./routes/configurar');
-// const criar_senha = require('./routes/criar_senha');
-// const escolha_seu_plano = require('./routes/escolha_seu_plano');
-// const forma_pagamento = require('./routes/forma_pagamento');
-// const login = require('./routes/login');
-// const planos = require('./routes/planos');
-// const configurar_cartao = require('./routes/configurar_cartao');
-// const criar_perfil = require('./routes/criar_perfil');
-// const primeiro_acesso_criar_perfil = require('./routes/primeiro_acesso_criar_perfil');
-
-
-// rotas
 const create_login = require('./routes/create_login');
 const adicionar_perfil = require('./routes/adicionar_perfil');
 const home_catalog = require('./routes/home_page_catalog');
@@ -36,10 +23,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(create_login);
 app.use(adicionar_perfil);
 app.use(home_catalog);
-// app.use(criar_perfil);
-
-// app.use(primeiro_acesso_criar_perfil);
-// app.use(adicionar_perfil);
 
 app.listen(8000, erro => {
 
