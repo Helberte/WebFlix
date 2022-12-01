1a22a648e0a835d30aa3baf67e315b6954696b19
helberte costa arruda <helberteads@gmail.com>
2022-11-30 21:13:32 -0400

    finalizando a parte de carregar o filme no catalogo

--- controllers/home_page_controller.js
+++ controllers/home_page_controller.js
@@ -5,13 +5,28 @@ const prisma = new PrismaClient();
 
 
 
-function homePage(req, res){
+async function homePage(req, res){
 
+  // pega todas as categorias
+  const categorias = await prisma.genero.findMany();
+  
 
+  // seleciona todos os filmes com o nome da categoria de cada um 
+  const filmes = await prisma.filme.findMany({
+    include: {
+      genero_filme: {
+        include:{
+          genero: {
+            select:{
+              nome: true,
+            },
+          },
+        },
+      },
+    },
+  });
 
-
-
-  res.render('home_page_catalog');
+  res.render('home_page_catalog', { filmes, categorias });
 }
 
 module.exports = { homePage };
